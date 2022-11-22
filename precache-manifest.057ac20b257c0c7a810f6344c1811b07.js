23868737fe61be39fa9b75ab48908ef80782c406
Durotoye <techsolutelab@gmail.com>
2022-11-22 06:51:15 +0200

    Updates

---
+++ precache-manifest.057ac20b257c0c7a810f6344c1811b07.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "819d81920c6fc812cc9c",
+    "url": "/weather-forecast/static/css/main.184a858e.chunk.css"
+  },
+  {
+    "revision": "819d81920c6fc812cc9c",
+    "url": "/weather-forecast/static/js/main.819d8192.chunk.js"
+  },
+  {
+    "revision": "7444bc21806ac70cbaf6",
+    "url": "/weather-forecast/static/js/1.7444bc21.chunk.js"
+  },
+  {
+    "revision": "f2d38ab6dc1f45e8eef0",
+    "url": "/weather-forecast/static/js/runtime~main.f2d38ab6.js"
+  },
+  {
+    "revision": "d54ed8355ddfc62e663c3e6f8151a216",
+    "url": "/weather-forecast/static/media/bg.d54ed835.png"
+  },
+  {
+    "revision": "9a1575b293049ea8a6cd4dba37aabd37",
+    "url": "/weather-forecast/index.html"
+  }
+];
